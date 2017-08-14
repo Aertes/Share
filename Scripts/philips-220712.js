5354f9ff386ee661ce84c771467231bd49de1a51
Aertes <mrxu1010@outlook.com>
2017-08-14 11:19:07 +0800

    Share

--- Scripts/philips-220712.js
+++ Scripts/philips-220712.js
@@ -6,7 +6,13 @@
             domain: "http://47.92.147.251/",
             localurl: encodeURIComponent(window.location.href),
             nametxt: { "txtc": "&copy;\u4f18\u4eab\u5ba2" },
-            popHeight: 445
+            popHeight: 445,
+            hotjson: {
+                "icons": [
+                    { "name": "新浪微博", "title": "分享到新浪微博", "style": "sina icon-xinlang-copy", "other": 1 },
+                    { "name": "微信朋友圈", "title": "分享到微信朋友圈", "style": "weixin icon-pengyouquan1", "other": 2 },
+                ]
+            }
         }
         var options = $.extend(defaults, options);
         var mvbg = $("<div>").addClass("in-share-cvbg modal");
@@ -17,7 +23,6 @@
             var surl = obj.attr("surl");
             var simg = obj.attr("simg");
             var stxt = obj.attr("stxt");
-            var stitle = null;
             var scount = obj.attr("scount");
             obj.html('loading...');
             //构造体
@@ -54,45 +59,11 @@
                 $("body")[0].appendChild(styleTag);
             }
             //分享按钮
-            $.getJSON(options.domain + "share/getsiteinfo", { 'siteid': siteid, 'time': new Date().getTime(), 'jsoncallback': '?' },
-                function (data) {
-                    //pageinfo.sitename = data.sitename;
-                    if(data.isweibo) {
-                        $("<a>").attr("href", "javascript:void(0)").attr("title", '').attr("platform", '').attr("platformname", '').addClass("in-share-icon1 iconfont sina icon-xinlang-copy").appendTo(mli);
-                        $("<div>").addClass("in-share-count").appendTo(mli);
-                        ma.append(mli);
-                    }
-                    if(data.isweixin) {
-                        $("<a>").attr("href", "javascript:void(0)").attr("title", '').attr("platform", '').attr("platformname", '').addClass("in-share-icon1 iconfont weixin icon-pengyouquan1").appendTo(mli);
-                        $("<div>").addClass("in-share-count").appendTo(mli);
-                        ma.append(mli);
-                    }
-                    if(data.sitetype == 1){
-                        surl = options.localurl;
-                        simg = data.sharepic1+','+data.sharepic2+','+data.sharepic3+','+data.sharepic4+','+data.sharepic5;
-                        stxt = data.sharecontent;
-                        stitle = data.sitename;
-                    }
-                    //初始化添加事件
-                    obj.find(".in-share-li a").not(".in-share-count").bind("click", function () {
-
-                        var vw = '600px';
-                        var vh = '435px';
-                        var tb = options.domain + 'Share/Index';
-                        if ($(this).hasClass('weixin')) { vw = '250px'; vh = '240px'; tb = options.domain + 'qrcode.html'; }
-                        if (isMobile() && !$(this).hasClass('weixin')) { vw = '100%'; vh = '350px'; }
-                        showshareboxIframe($(this).attr("platform"), $(this).attr("platformname"), surl, simg, stxt,stitle, vh, vw, options.domain, siteid, tb);
-
-                        mvbg.fadeIn();
-
-
-                    });
-            });
-            /*$.each(options.hotjson["icons"], function (index, value) {
+            $.each(options.hotjson["icons"], function (index, value) {
                 $("<a>").attr("href", "javascript:void(0)").attr("title", value.title).attr("platform", value.other).attr("platformname", value.name).addClass("in-share-icon1 iconfont " + value.style).appendTo(mli);
                 $("<div>").addClass("in-share-count").appendTo(mli);
                 ma.append(mli);
-            });*/
+            });
             obj.empty();
             //ma.append(mli).append($("<li>").addClass("in-share-count").append($("<a>").attr("href", "javascript:void(0)").attr("class", 'num'))).appendTo(obj);
             ma.appendTo(obj);
@@ -126,8 +97,46 @@
                 function (data) {
                     obj.find(".in-share-count").first().html('<a href="javascript:void(0)" class="num">' + data.sinacount + '</a>');
                     obj.find(".in-share-count").last().html('<a href="javascript:void(0)" class="num">' + data.weibocount + '</a>');
+                    /*$.each(data["share"], function (index, value) {
+                    obj.find(".in-share-count").html(value.sharenum);
+                    if (value.bigpic != "") {
+                    obj.find(".in-share-ad").append($("<img>").attr("src", value.bigpic).attr("alt", value.bigpicalt));
+                    if (value.bigpiclink != "") {
+                    obj.find(".in-share-ad").wrapInner($("<a>").attr("href", value.bigpiclink).attr("target", "_blank"));
+                    }
+                    obj.find(".in-share-ad").show();
+                    adimg.src = value.bigpic;
+                    adpic = "ok";
+                    }
+                    if (value.smallpic != "") {
+                    opentype = (value.opentype != "") ? value.opentype : "click";
+                    ccimg.src = value.smallpic + "?" + new Date().getTime();
+                    $(ccimg).load(function () {
+                    $("<a>").addClass("in-share-more").attr("href", "javascript:void(0)").attr("title", value.smallpicalt).css("background-image", "url(" + value.smallpic + ")").css("width", ccimg.width).bind(opentype, function () {
+                    obj.find('.in-share-box').show();
+                    $('.a-share').css('zIndex', '');
+                    obj.css('zIndex', 100000);
+                    }).show().insertBefore(obj.find(".in-share-count"));
+                    ccimg.src = null;
+                    });
+    
+                    }
+                    });*/
+                });
+            //初始化添加事件
+            obj.find(".in-share-li a").not(".in-share-count").bind("click", function () {
+
+                var vw = '600px';
+                var vh = '435px';
+                var tb = options.domain + 'Share/Index';
+                if ($(this).hasClass('weixin')) { vw = '250px'; vh = '240px'; tb = options.domain + 'qrcode.html'; }
+                if (isMobile() && !$(this).hasClass('weixin')) { vw = '100%'; vh = '350px'; }
+                showshareboxIframe($(this).attr("platform"), $(this).attr("platformname"), surl, simg, stxt, vh, vw, options.domain, siteid, tb);
+
+                mvbg.fadeIn();
+
+
             });
-            
             $(window).bind("resize scroll", function () { showshareboxIframePosition(mc) });
 
 
@@ -209,7 +218,7 @@ function getUrlParam(name) {
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数
     if (r != null) return unescape(r[2]); return null; //返回参数值
 }
-function showshareboxIframe(i, iname, surl, simg, stxt,stitle, sheight, swidth, sdomain, siteid, tb) {
+function showshareboxIframe(i, iname, surl, simg, stxt, sheight, swidth, sdomain, siteid, tb) {
     var adpic = '';
     if (!havecheck) {
         $('img').not(".in-share-ad img").each(function (index) {
@@ -235,7 +244,7 @@ function showshareboxIframe(i, iname, surl, simg, stxt,stitle, sheight, swidth, 
     $(".in-share-iframeloading").show();
     $("#in-share-iframe").attr("height", sheight);
     $("#in-share-iframe").attr("width", swidth);
-    var pagetitle = (stitle!=null)?encodeURI(stitle):encodeURI(document.title);
+    var pagetitle = encodeURI(document.title);
     var pagedisc = encodeURI($('meta[name=description]').attr("content"));
     var url = "";
     var rurl = "";
